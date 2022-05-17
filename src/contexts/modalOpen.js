e00bee9c255244f0bcf803cf4ed601818ebdbed0
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-05-17 17:11:10 -0300

    fix items

--- src/contexts/modalOpen.js
+++ src/contexts/modalOpen.js
@@ -6,9 +6,10 @@ export function ModalContextProvider(props) {
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [type, setType] = useState('album')
     const [album_id, setAlbum_id] = useState('album')
+    const [track_id, setTrack_id] = useState('')
     const [modalParams, setModalParams] = useState({albumName: "", albumYear:""}) 
   return (
-    <IsModalOpenContext.Provider value={{isModalOpen, setIsModalOpen, type, setType, album_id, setAlbum_id, modalParams, setModalParams}}>
+    <IsModalOpenContext.Provider value={{isModalOpen, setIsModalOpen, type, setType, album_id, setAlbum_id, modalParams, setModalParams, track_id, setTrack_id}}>
         {props.children}
     </IsModalOpenContext.Provider>
   )
